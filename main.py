7608c6893c0932bda28a4c8b8c9b37bfe7cad1f1
Austin Scott <xeirxes@gmail.com>
2016-12-03 15:35:53 -0800

    Got a map to display now, but doesn't read data from random map.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Import Python libraries
+import random
+
 # Import pygame
 import pygame
 from pygame.locals import *
@@ -8,44 +11,45 @@ from pygame.locals import *
 def main():
     # Initialise screen
     pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Discover')
 
-    # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((255, 255, 255))
+    # Render full map
+    map_data = create_map_data()
+    map = render_map(map_data)
 
-    # Blit everything to the screen
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
-
-    # Draw a test box
-    box = draw_box(width=screen.get_width()*0.9,
-                   height=screen.get_height()/10,
-                   fillcolor=(240,240,240),
-                   bordercolor=(40,40,40))
-    box_spacing = (screen.get_width() - box.get_width()) / 2
-    background.blit(box,
-                    (box_spacing, screen.get_height() - box.get_height() - box_spacing))
-
-    # Event loop
     while 1:
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
-            elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                    quit()
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                exit()
 
-        screen.blit(background, (0, 0))
+        screen.blit(map, (0, 0))
         pygame.display.flip()
 
 
-def draw_box(width, height, borderwidth=3, fillcolor=(255, 255, 255), bordercolor=(25,25,25)):
-    new_surface = pygame.Surface((width, height))
-    new_surface.fill(fillcolor)
-    pygame.draw.rect(new_surface, bordercolor, new_surface.get_rect(), borderwidth)
-    return new_surface
+def create_map_data():
+    return None
+
+
+def render_map(data):
+    tile_size = 5
+    grid_size = 100
+    water_tile = pygame.Surface((tile_size, tile_size))
+    water_tile.fill((25,25,125))
+    land_tile = pygame.Surface((tile_size, tile_size))
+    land_tile.fill((25,125,25))
+    surface = pygame.Surface((grid_size * tile_size, grid_size * tile_size)).convert()
+    for y in range(grid_size):
+        for x in range(grid_size):
+            blitsource = None
+            if random.randint(0, 1):
+                blitsource = water_tile
+            else:
+                blitsource = land_tile
+            surface.blit(blitsource, (x * tile_size, y * tile_size))
+    return surface
 
 
 if __name__ == '__main__':
