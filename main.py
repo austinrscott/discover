ca38db206e236eca067e348cedd702bcb36a760e
Austin Scott <xeirxes@gmail.com>
2016-12-03 20:59:02 -0800

    Map now detects what cell has been clicked relative to where the map has been drawn.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     root = ContainerWidget(screen.get_rect())
 
     # Initialize map
-    root.add(Map())
+    root.add(Map(pos=[25, 25]))
 
     # Event loop
     while 1:
@@ -36,7 +36,6 @@ def main():
 class Widget():
     def __init__(self, rect):
         self._rect = pygame.Rect(rect)
-        self._surface = None
 
     def event(self, e):
         pass
@@ -46,6 +45,10 @@ class Widget():
 
     def get_surface(self):
         return self._surface
+
+    def _event_get_new_pos(self, pos):
+        new_pos = [pos[0] - self._rect.left, pos[1] - self._rect.top]
+        return new_pos
 
     def render(self):
         pass
@@ -64,6 +67,7 @@ class ContainerWidget(Widget):
         self._widgets.append(widget)
 
     def event(self, e):
+        e.pos = self._event_get_new_pos(e.pos)
         for widget in self._widgets:
             widget.event(e)
 
@@ -75,19 +79,25 @@ class ContainerWidget(Widget):
 
 
 class Map(Widget):
-    def __init__(self, rect=None):
-        self._tile_size = 4
-        self._grid_size = [145,145]
+    def __init__(self, rect=None, pos=None):
+        self._tile_size = 8
+        self._grid_size = [70, 70]
         self._map_obj = RandomTextMap(width=self._grid_size[0],
                                       height=self._grid_size[1],
                                       water_chance=0.01,
                                       num_island_seeds=40,
                                       land_water_ratio=0.4)
+        self._create_map_surface()
+        new_rect = self._surface.get_rect()
+        if pos:
+            new_rect.move_ip(pos[0], pos[1])
+        super().__init__(new_rect)
         self.render()
-        super().__init__(self._surface.get_rect())
+
 
     def event(self, e):
-        if e.type == MOUSEBUTTONDOWN and e.button == 1:
+        if e.type == MOUSEBUTTONDOWN and e.button == 1 and self._rect.collidepoint(e.pos):
+            e.pos = self._event_get_new_pos(e.pos)
             cell_clicked = self._find_cell(e.pos)
             print(cell_clicked)
 
@@ -95,16 +105,17 @@ class Map(Widget):
         x, y = pos[0], pos[1]
         return [x // self._tile_size, y // self._tile_size]
 
+    def _create_map_surface(self):
+        self._surface = pygame.Surface((self._grid_size[0] * self._tile_size, self._grid_size[1] * self._tile_size))
+
     def render(self):
         matrix = self._map_obj.output_map()
-        new_surface = pygame.Surface((self._grid_size[0] * self._tile_size, self._grid_size[1] * self._tile_size))
-        new_surface.fill((25, 25, 125))
+        self._surface.fill((25, 25, 125))
         for y in range(self._grid_size[1]):
             for x in range(self._grid_size[0]):
                 if matrix[y][x] == '#':
-                    new_surface.fill((25, 125, 25),
+                    self._surface.fill((25, 125, 25),
                                      (x * self._tile_size, y * self._tile_size, self._tile_size, self._tile_size))
-        self._surface = new_surface
 
 
 if __name__ == '__main__':
